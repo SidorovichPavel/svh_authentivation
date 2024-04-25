3f70dcdf211fdbe60417ff255d2a5c9aacee9360
MRV <sabudilovskiy@yandex.ru>
2024-04-25 19:59:36 +0000

    format

--- src/models/error_response.hpp
+++ src/models/error_response.hpp
@@ -2,13 +2,14 @@
 #include <string>
 #include <uopenapi/enum_helpers/declare.hpp>
 
-namespace models{
+namespace model{
     UOPENAPI_DECLARE_ENUM(ErrorCode,
                           int,
                           invalid_token,
                           username_busy,
                           invalid_credentials
                           );
+                          
     struct ErrorResponseBody{
         std::string message;
         ErrorCode error_code;
