2cb9bda067547bdd3942cd545a39adcc3afaa55a
MRV <sabudilovskiy@yandex.ru>
2024-04-27 19:03:02 +0000

    impl register and permitions db

--- postgresql/users_manager_db/000_init.sql
+++ postgresql/users_manager_db/000_init.sql
@@ -1,6 +1,6 @@
 -- Database generated with pgModeler (PostgreSQL Database Modeler).
 -- pgModeler version: 1.1.2
--- PostgreSQL version: 12.0
+-- PostgreSQL version: 16.0
 -- Project Site: pgmodeler.io
 -- Model Author: ---
 
@@ -29,9 +29,11 @@ CREATE TABLE svh_users_manager.users (
 	first_name text NOT NULL,
 	last_name text NOT NULL,
 	nickname text NOT NULL,
+	age smallint NOT NULL,
 	password_hash text NOT NULL,
-	age bigint NOT NULL,
-	CONSTRAINT users_pk PRIMARY KEY (id)
+	salt text NOT NULL,
+	CONSTRAINT users_pk PRIMARY KEY (id),
+	CONSTRAINT unique_nickname UNIQUE (nickname)
 );
 -- ddl-end --
 
@@ -39,10 +41,42 @@ CREATE TABLE svh_users_manager.users (
 DROP TYPE IF EXISTS svh_users_manager."UserCredentialsType" CASCADE;
 CREATE TYPE svh_users_manager."UserCredentialsType" AS
 (
- first_name text,
+ user_name text,
  last_name text,
  nickname text,
  password_hash text,
- age bigint
+ age smallint
 );
+-- ddl-end --
+
+-- object: svh_users_manager.permitions | type: TABLE --
+DROP TABLE IF EXISTS svh_users_manager.permitions CASCADE;
+CREATE TABLE svh_users_manager.permitions (
+	id integer NOT NULL,
+	title text NOT NULL,
+	CONSTRAINT permitions_pk PRIMARY KEY (id)
+);
+-- ddl-end --
+
+-- object: svh_users_manager.permitions_to_users | type: TABLE --
+DROP TABLE IF EXISTS svh_users_manager.permitions_to_users CASCADE;
+CREATE TABLE svh_users_manager.permitions_to_users (
+	id_users uuid,
+	id_permitions integer
+
+);
+-- ddl-end --
+
+-- object: users_fk | type: CONSTRAINT --
+ALTER TABLE svh_users_manager.permitions_to_users DROP CONSTRAINT IF EXISTS users_fk CASCADE;
+ALTER TABLE svh_users_manager.permitions_to_users ADD CONSTRAINT users_fk FOREIGN KEY (id_users)
+REFERENCES svh_users_manager.users (id) MATCH FULL
+ON DELETE SET NULL ON UPDATE CASCADE;
+-- ddl-end --
+
+-- object: permitions_fk | type: CONSTRAINT --
+ALTER TABLE svh_users_manager.permitions_to_users DROP CONSTRAINT IF EXISTS permitions_fk CASCADE;
+ALTER TABLE svh_users_manager.permitions_to_users ADD CONSTRAINT permitions_fk FOREIGN KEY (id_permitions)
+REFERENCES svh_users_manager.permitions (id) MATCH FULL
+ON DELETE SET NULL ON UPDATE CASCADE;
 -- ddl-end --
